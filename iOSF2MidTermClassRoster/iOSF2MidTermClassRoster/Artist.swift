47ef0fc9a36c974fe85a4227124f0186aabfffc1
dkoch <dkoch7591@gmail.com>
2014-11-16 19:18:58 -0800

    Added 'tiff' files

---
+++ iOSF2MidTermClassRoster/iOSF2MidTermClassRoster/Artist.swift
@@ -0,0 +1,18 @@
+//
+//  Artist.swift
+//  iOSF2MidTermClassRoster
+//
+//  Created by Gru on 11/15/14.
+//  Copyright (c) 2014 GruTech. All rights reserved.
+//
+
+import Cocoa
+
+class Artist: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
